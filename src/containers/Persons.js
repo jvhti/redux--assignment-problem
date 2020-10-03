4e3792ee15d48e24b9d07a900bd8467f9d26bdda
jvhti <jvhti@hotmail.com>
2020-10-02 23:23:30 -0300

    Using local UI state and redux state

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -7,11 +7,11 @@ import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    personAddedHandler = () => {
+    personAddedHandler = (name, age) => {
         const newPerson = {
             id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor(Math.random() * 40)
+            name,
+            age
         }
         this.props.onPersonAdded(newPerson);
     }
