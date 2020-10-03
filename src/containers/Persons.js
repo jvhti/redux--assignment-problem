01ff085056a7b281f304540d469e796b37695006
jvhti <jvhti@hotmail.com>
2020-10-02 23:13:48 -0300

    Apply redux to remove local state from Persons component

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,46 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+import * as actionTypes from '../store/actions';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
     personAddedHandler = () => {
         const newPerson = {
             id: Math.random(), // not really unique but good enough here!
             name: 'Max',
             age: Math.floor(Math.random() * 40)
         }
-        this.setState((prevState) => {
-            return {persons: prevState.persons.concat(newPerson)}
-        });
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState((prevState) => {
-            return {persons: prevState.persons.filter(person => person.id !== personId)}
-        });
+        this.props.onPersonAdded(newPerson);
     }
 
     render() {
         return (
             <div>
                 <AddPerson personAdded={this.personAddedHandler}/>
-                {this.state.persons.map(person => (
+                {this.props.persons.map(person => (
                     <Person
                         key={person.id}
                         name={person.name}
                         age={person.age}
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.onPersonDeleted(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const mapStateToProps = (state) => ({
+    persons: state.persons
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onPersonAdded: (person) => dispatch({type: actionTypes.ADD_PERSON, payload: person}),
+        onPersonDeleted: (id) => dispatch({type: actionTypes.DELETE_PERSON, id})
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
